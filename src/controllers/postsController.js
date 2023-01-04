794b2cdb0fc66c6310628d11ddbe46847f608812
nandasino <fernandamsino@gmail.com>
2023-01-04 19:39:37 -0300

    middleware de verificação de token e get Posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,12 +1,21 @@
 import { postRepository } from "../repositories/postsRepository.js";
 
 export  async function postPost (req,res){
-    const user = res.locals.user;
-    const {link,caption} = res.locals.postForm;
+    const user_id = res.locals.userId;
+    const { link, caption } = req.body;
     try {
         await postRepository.insertPost(user_id,link,caption);
-        res.sendStatus(200);
-        
+        res.sendStatus(200); 
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export  async function getPosts (req,res){
+    try {
+        const result = await postRepository.selectPosts();
+        res.status(200).send(result.rows)
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
