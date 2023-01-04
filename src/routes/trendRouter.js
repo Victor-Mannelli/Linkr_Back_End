bcad782e3786526c2d06570d82fa991dc86395dd
Pedro-Arantes <developer9pedro@gmail.com>
2023-01-04 17:56:01 -0300

    feat: trendingController,trendRepository,trendRouter adding code

--- src/routes/trendRouter.js
+++ src/routes/trendRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getTrending } from "../controllers/trendingController.js";
+import { getTrending,getTrendingPosts } from "../controllers/trendingController.js";
 import { getTrendMd } from "../middlewares/getTrendingMiddleware.js";
 
 const router = Router();
 
-router.get("/hashtag/:hashtag",getTrending);
+router.get("/hashtag/:hashtag",getTrendingPosts);
 router.get("/hashtag",getTrending);
 
 export default router;
