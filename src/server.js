7a6f7bf9328ba5c14980aa245464c8cdacda13c6
nandasino <fernandamsino@gmail.com>
2023-01-05 13:32:49 -0300

    select post mais recente e limite de 20, import dotenv no server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import trendRouter  from "../src/routes/trendRouter.js";
+import dotenv from 'dotenv';
+//import trendRouter  from "../src/routes/trendRouter.js";
+import trendRouter from './routes/trendRouter.js';
+import postRouter from './routes/postRouter.js';
+import authRouter from './routes/authRouter.js'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(trendRouter)
+app.use(trendRouter);
+app.use(postRouter);
+app.use(authRouter);
 
+dotenv.config();
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
