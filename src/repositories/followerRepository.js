33fbbc2c2b3252515ec15648c3417bc6443f88df
Pedro-Arantes <developer9pedro@gmail.com>
2023-01-11 15:54:40 -0300

    feat: follow Router,middleware,controller and repository

---
+++ src/repositories/followerRepository.js
@@ -0,0 +1,11 @@
+import { connection } from "../database/db.js";
+
+export async function insertFollow(follow_id,user_id) {
+
+    return connection.query('INSERT INTO followers (follow_id,user_id) VALUES ($1,$2) RETURNING id',[follow_id,user_id]);
+}
+
+export async function unFollow(id) {
+
+    return connection.query('DELETE FROM followers WHERE id = $1',[id]);
+}
