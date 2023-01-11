7676d4a3d2605775a9995370cb1b4d546e12b1e3
Pedro-Arantes <developer9pedro@gmail.com>
2023-01-11 17:20:32 -0300

    refact: trend query repository

--- src/repositories/trendRepository.js
+++ src/repositories/trendRepository.js
@@ -4,9 +4,14 @@ async function getTrend() {
 	return connection.query('SELECT * FROM trending ORDER BY count DESC LIMIT 10');
 }
 async function getTrendPost(trend) {
-	return connection.query('SELECT p.user_id, p.link ,p.caption, p."createdAt" AS date,tr.trending_name AS trend FROM post_trending  pt  JOIN posts p ON  pt.post_id = p.id JOIN trending tr ON pt.trending_id = tr.id WHERE tr.trending_name = $1 ORDER BY p."createdAt" DESC',["#"+trend]);
+	return connection.query('SELECT p.*, p."createdAt" AS date,u.profile_picture as "image",tr.trending_name AS trend FROM post_trending  pt  JOIN posts p ON  pt.post_id = p.id JOIN trending tr ON pt.trending_id = tr.id  JOIN users u  ON p.user_id = u.id WHERE tr.trending_name = $1 ORDER BY p."createdAt" DESC LIMIT 20',["#"+trend]);
 }
-
+`SELECT posts.*,
+    users.username, users.profile_picture as "image"
+    FROM posts 
+    JOIN users 
+    ON posts.user_id = users.id 
+    ORDER BY posts.id DESC LIMIT 20`
 export const trendRepository = {
 	getTrend,
 	getTrendPost
