924d5b04bede1ab9ccb404cf8f996d5f43055e20
AnaEstacheski <flaviaestacheski@gmail.com>
2023-01-12 17:16:11 -0300

    fix: server router conflict

--- src/routes/serverRouter.js
+++ src/routes/serverRouter.js
@@ -3,14 +3,11 @@ import authRouter from "./authRouter.js";
 import trendRouter from "./trendRouter.js";
 import userRouter from "./userRouter.js";
 import likesRouter from "./likesRouter.js";
-<<<<<<< Updated upstream
 import  followRouter from "./followRouter.js";
 
 //import postRouter from "./postRouter.js";
 
-=======
 import postRouter from "./postRouter.js";
->>>>>>> Stashed changes
 
 const serverRouter = Router();
 
@@ -18,10 +15,7 @@ serverRouter.use(authRouter);
 serverRouter.use(trendRouter);
 serverRouter.use(userRouter);
 serverRouter.use(likesRouter);
-<<<<<<< Updated upstream
 serverRouter.use(followRouter);
-=======
 serverRouter.use(postRouter);
->>>>>>> Stashed changes
 
 export default serverRouter;
